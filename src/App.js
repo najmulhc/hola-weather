a58868118385abc906256678943d323f117638f0
kridi32 <kridi32@outlook.com>
2022-04-07 20:31:40 +0600

    api installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
- 
+ import useWeather from './hooks/useWeather';
+ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 
 function App() {
+  const [cityName, setCityName] = useState();
+  const [weather,setWeather] = useWeather(cityName); 
+  
+  const handleSearchBtn =()=>{
+    console.log(weather);
+  }
   return (
     <div className=" app ">
-       <Header></Header>
-  
+       <Header
+       setCityName={setCityName}
+       handleSearchBtn={handleSearchBtn}
+       ></Header>
+      { 
+        weather.weather? <h1>weather ache</h1>:<h1>no weather data found yet</h1>
+      }
     </div>
   );
 }
---
+++ src/hooks/useWeather.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+
+const useWeather = cityName =>{
+    const [weather, setWeather] = useState({});
+    const apiKey = '4ae6fb068cc2205edcc3036059a84357'
+    const link = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    console.log(link);
+    useEffect( ()=>{
+        fetch(link)
+        .then(res => res.json())
+        .then(data => setWeather(data))
+    },[link])
+    return [weather, setWeather];
+}
+export default useWeather;
