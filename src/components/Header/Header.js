9691d83cbf85ecfd4e1b80f553df30a195dc1776
kridi32 <kridi32@outlook.com>
2022-04-07 19:41:40 +0600

    basic header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='glass-card py-8 px-20 rounded-none'>
+            <h1 className='text-3xl text-left text-slate-800 '>WeatherApp</h1>
+        </header>
+    );
+};
+
+export default Header;
